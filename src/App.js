583ce2d69b89b2338a5ea1e9d56138e9d9557256
Murari Gupta <alexgupta2367@gmail.com>
2023-07-21 21:27:36 +0545

    worktime tracker component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import WorkerTimeTracker from './pages/WorkerTimeTracker';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleCount = () => {
+    setCount((count) => count + 1);
+    console.log({ count });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Workers Time Tracker</h2>
-      </header>
+      <section>{count}</section>
+      <button onClick={handleCount}>Click Me</button>
+      <h2>Workers Time Tracker</h2>
+      <WorkerTimeTracker />
     </div>
   );
 }
