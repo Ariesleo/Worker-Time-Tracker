d876b2f70b61c93c9193d3b1d38883c2b4d1fd26
Murari Gupta <alexgupta2367@gmail.com>
2023-07-24 09:13:30 +0545

    created dropdown component

--- src/components/DropDown/Dropdown.js
+++ src/components/DropDown/Dropdown.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
-const Dropdown = () => {
+const Dropdown = ({ handleOnChnage }) => {
   const reportFrequency = ['Weekly', 'Bi-Weekly', 'Monthly', 'Bi-Monthly'];
-  // console.log({ value });
-
-  // Check if the value prop exists in the reportFrequency array
-  // const selectedValue = reportFrequency.includes(value) ? value : '';
-
-  const handleReportFrequency = (event) => {
-    // setSelectedOption(event.target.value);
-    console.log(event.target.value);
-  };
 
   return (
     <div>
-      <select onChange={handleReportFrequency}>
-        <option disabled>Report Frequency</option>
+      <select onChange={handleOnChnage} defaultValue="">
+        <option value="" disabled hidden>
+          Report Frequency
+        </option>
         {reportFrequency.map((option, index) => {
           return <option key={index}>{option}</option>;
         })}
