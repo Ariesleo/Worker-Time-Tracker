3e9d86cb08c18d216d9210bdea3e125f94e11a45
Murari Gupta <alexgupta2367@gmail.com>
2023-07-24 09:27:43 +0545

    removing button and adding some css to it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { useState } from 'react';
 import './App.css';
 import WorkerTimeTracker from './pages/WorkerTimeTracker';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleCount = () => {
-    setCount((count) => count + 1);
-    console.log({ count });
-  };
   return (
     <div className="App">
-      <section>{count}</section>
-      <button onClick={handleCount}>Click Me</button>
-      <h2>Workers Time Tracker</h2>
       <WorkerTimeTracker />
     </div>
   );
